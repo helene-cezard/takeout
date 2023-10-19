d6692d90bc172d2bd319e2275b45236b72edb2b8
helene-cezard <helene.cezard@gmail.com>
2023-10-19 13:49:18 +0200

    basket icon

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -1,10 +1,14 @@
 import '../styles/Banner.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faBasketShopping } from '@fortawesome/free-solid-svg-icons'
+
 
 
 function Banner({isOpen, setIsOpen}) {
+  const element = <FontAwesomeIcon icon={faBasketShopping} />
   return (
   <header className="banner">
-    <button onClick={() => setIsOpen(!isOpen)}>Panier</button>
+    <button className='banner__basket-button' onClick={() => setIsOpen(!isOpen)}>{element}</button>
     <h1>Viva Italia</h1>
   </header>
   );
