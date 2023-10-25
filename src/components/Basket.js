3eb29d10fa9b65cb8cc4111a254cb8ead5976439
helene-cezard <helene.cezard@gmail.com>
2023-10-25 20:15:27 +0200

    html, css and mobile first

--- src/components/Basket.js
+++ src/components/Basket.js
@@ -30,26 +30,26 @@ function updateAmount(name, value) {
       <button className='basket__close-button' onClick={() => setIsOpen(false)}>X</button>
       <h2 className='basket__title'>Panier</h2>
       {basket.length > 0 ? (
-        <React.Fragment>
+        <div className='basket__content'>
           <ul>
           {basket.map(({name, price, amount}, index) =>
           (
             <li key={`${name}-${index}`} className='basket__items'>
               {name} :
               <span
-              className='basket__prices'>{price}€ x
-              <input className='basket__amount' type='number' value={`${amount}`} min={0}
-              onBlur={() => amount === '0' && removeFromBasket(name)}
-              onChange={(event) => updateAmount(name, event.target.value)}
+              className='basket__prices'>{price}€ x <input 
+                className='basket__amount' type='number' value={`${amount}`} min={0}
+                onBlur={() => amount === '0' && removeFromBasket(name)}
+                onChange={(event) => updateAmount(name, event.target.value)}
               />
               </span>
                <button onClick={() => removeFromBasket(name)}>X</button>
             </li>
           ))}
-          <li className='basket__total'>Total : <span className='basket__prices'>{total}€</span></li>
-                </ul>
-                <button className='basket__empty-button' onClick={() => updateBasket([])}>Vider le panier</button>
-        </React.Fragment>
+            <li className='basket__total'>Total : <span className='basket__prices'>{total}€</span></li>
+          </ul>
+          <button className='basket__empty-button' onClick={() => updateBasket([])}>Vider le panier</button>
+        </div>
       ) : (
         <p>Votre panier est vide</p>
       )}
